182aea1e8f7b8b0dad8b6be353d438c7dd90684c
Nada El-Falou <nadafalou2001@gmail.com>
2023-02-21 18:27:39 +0000

    converted operations to be in float16 and/or __half

--- mmul.cu
+++ mmul.cu
@@ -7,6 +7,7 @@
 #include <string.h>
 #include <assert.h>
 #include <sys/time.h>
+#include "fp16_conversion.h"
 
 
 double gettime() {
@@ -18,21 +19,22 @@ double gettime() {
 
 // TEMPORARY FUNCTION
 // Fill the array A(nr_rows_A, nr_cols_A) with random numbers on CPU
-void fill_ones(float *A, int n_elements) {
+void fill_ones(__half *A, int n_elements) {
 	for(int i = 0; i < n_elements; i++) {
-		float num = 1.0;
+		__half num = 1.0;
 		A[i] = num;
 	}
 }
 
 
 //Print matrix A(nr_rows_A, nr_cols_A) storage in column-major format
-void print_matrix(const float *A, int nr_rows_A, int nr_cols_A, int nr_depth_A) {
+void print_matrix(const __half *A, int nr_rows_A, int nr_cols_A, int nr_depth_A) {
 	for (int k = 0; k < nr_depth_A; k++) {
 		std::cout << "k = " << k << std::endl;
 		for(int i = 0; i < nr_rows_A; ++i){
 			for(int j = 0; j < nr_cols_A; ++j){
-				std::cout << A[k * nr_rows_A * nr_cols_A + j * nr_rows_A + i] << " ";
+				std::cout << (float) A[k * nr_rows_A * nr_cols_A + j * nr_rows_A + i];
+				std::cout << " ";
 			}
 			std::cout << std::endl;
 		}
@@ -43,8 +45,6 @@ void print_matrix(const float *A, int nr_rows_A, int nr_cols_A, int nr_depth_A) 
 
 
 int main( int argc, char *argv[] ) {
-	// Allocate 3 arrays on CPU
-	// num rows and cols
 	size_t F, B, rho, T;
 	
 	F = 256;
@@ -70,12 +70,12 @@ int main( int argc, char *argv[] ) {
 	cublasHandle_t handle;
 	cublasCreate(&handle);
 	
-	float alpha = 1;
-	float beta = 0;
+	__half alpha = 1;
+	__half beta = 0;
 
-	float *h_Iin = (float *)malloc(F * T * rho * sizeof(float));
-	float *h_W = (float *)malloc(F * rho * B * sizeof(float));
-	float *h_Iout = (float *)malloc(F * T * B * sizeof(float));
+	__half *h_Iin = (__half *)malloc(F * T * rho * sizeof(__half));
+	__half *h_W = (__half *)malloc(F * rho * B * sizeof(__half));
+	__half *h_Iout = (__half *)malloc(F * T * B * sizeof(__half));
 	if (h_Iin == NULL) {
 		std::cout << "Iin Malloc failed!" << std::endl;
 		return;
@@ -114,11 +114,11 @@ int main( int argc, char *argv[] ) {
 	}
 
 	// Allocate 3 arrays on GPU
-	float *d_Iin, *d_W, *d_Iout;
-	int feedback1 = cudaMalloc(&d_Iin, T * rho * sizeof(float));
-	int feedback2 = cudaMalloc(&d_W, rho * B * sizeof(float));
-	int feedback3 = cudaMalloc(&d_Iout, T * B * sizeof(float));
-	if (feedback1 != 0 or feedback2 != 0 or feedback3 != 0) {
+	__half *d_Iin, *d_W, *d_Iout;
+	int stat1 = cudaMalloc(&d_Iin, T * rho * sizeof(__half));
+	int stat2 = cudaMalloc(&d_W, rho * B * sizeof(__half));
+	int stat3 = cudaMalloc(&d_Iout, T * B * sizeof(__half));
+	if (stat1 != 0 or stat2 != 0 or stat3 != 0) {
 		std::cout << "cudaMalloc failed!" << std::endl;
 		return;
 	}
@@ -129,15 +129,14 @@ int main( int argc, char *argv[] ) {
 
 	for (int f = 0; f < F; f++) {	
 		// Copy data from CPU to GP
-		cudaMemcpy(d_Iin,h_Iin + f * T * rho, T * rho * sizeof(float),cudaMemcpyHostToDevice);
-		assert(f * rho * B < sizeof(float) * F * rho * B);
-		cudaMemcpy(d_W,h_W + f * rho * B, rho * B * sizeof(float),cudaMemcpyHostToDevice);
+		cudaMemcpy(d_Iin,h_Iin + f * T * rho, T * rho * sizeof(__half),cudaMemcpyHostToDevice);
+		cudaMemcpy(d_W,h_W + f * rho * B, rho * B * sizeof(__half),cudaMemcpyHostToDevice);
 
 		// Multiply A and B^T on GPU
-		cublasSgemm(handle, CUBLAS_OP_N, CUBLAS_OP_T, T, B, rho, &alpha, d_Iin, T, d_W, B, &beta, d_Iout, T);
+		cublasHgemm(handle, CUBLAS_OP_N, CUBLAS_OP_T, T, B, rho, &alpha, d_Iin, T, d_W, B, &beta, d_Iout, T);
 
 		// Copy the result on host memory
-		cudaMemcpy(h_Iout + f * T * B, d_Iout,T * B * sizeof(float),cudaMemcpyDeviceToHost);
+		cudaMemcpy(h_Iout + f * T * B, d_Iout,T * B * sizeof(__half),cudaMemcpyDeviceToHost);
 
 	}
 
